66c871462f171d49f9189a8f677cab913524af2c
Kusti Kaljo <kusti.kaljo@tptlive.ee>
2025-05-05 19:42:11 +0300

    Fixed videogrids

--- src/components/Videogrid.vue
+++ src/components/Videogrid.vue
@@ -15,22 +15,22 @@
   
   const videos = [
     // Row 1
-    { title: "Funny Cat Video", views: "1M views • 1 day ago", img: "https://placekitten.com/320/180" },
+    { title: "Funny Bear Video", views: "1M views • 1 day ago", img: "https://placebear.com/200/301" },
     { title: "Bear Adventures", views: "500K views • 2 days ago", img: "https://placebear.com/320/180" },
-    { title: "Another Cat Video", views: "300K views • 5 days ago", img: "https://placekitten.com/321/180" },
+    { title: "Another Bear Video", views: "300K views • 5 days ago", img: "https://placebear.com/352/180" },
     { title: "Bear Comedy", views: "200K views • 1 week ago", img: "https://placebear.com/321/180" },
     
     // Row 2
-    { title: "Tech Reviews", views: "800K views • 3 days ago", img: "https://placekitten.com/322/180" },
+    { title: "Tech Reviews", views: "800K views • 3 days ago", img: "https://placebear.com/331/180" },
     { title: "Travel Vlog", views: "2M views • 1 week ago", img: "https://placebear.com/322/180" },
-    { title: "Cooking Show", views: "900K views • 2 weeks ago", img: "https://placekitten.com/323/180" },
+    { title: "Cooking Show", views: "900K views • 2 weeks ago", img: "https://placebear.com/125/180" },
     { title: "Gaming Highlights", views: "1.2M views • 5 days ago", img: "https://placebear.com/323/180" },
     
     // Row 3
-    { title: "DIY Crafts", views: "700K views • 4 days ago", img: "https://placekitten.com/324/180" },
+    { title: "DIY Crafts", views: "700K views • 4 days ago", img: "https://placebear.com/421/180" },
     { title: "Movie Trailers", views: "5M views • 2 days ago", img: "https://placebear.com/324/180" },
-    { title: "Workout Routine", views: "600K views • 1 week ago", img: "https://placekitten.com/325/180" },
-    { title: "Art Time-lapse", views: "850K views • 3 days ago", img: "https://placebear.com/325/180" }
+    { title: "Workout Routine", views: "600K views • 1 week ago", img: "https://placebear.com/351/180" },
+    { title: "Art Time-lapse", views: "850K views • 3 days ago", img: "https://placebear.com/523/180" }
   ]
   </script>
   
